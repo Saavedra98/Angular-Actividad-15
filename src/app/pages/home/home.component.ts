9c4ef37870dc24eb3480a51960d9264025d51a9c
Saavedra98 <saavedraerazoc@gmail.com>
2023-01-11 07:05:44 -0500

    actualizado

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  personas:{nombre:string, apellido:string, carrera:string}[]=[]
+  nombre='';
+  apellido='';
+  carrera='';
+
+  
+
+  registrar(){
+    this.personas.push({'nombre':this.nombre, 'apellido':this.apellido, 'carrera':this.carrera})
+
+    this.nombre = "";
+    this.apellido = "";
+    this.carrera = "";
+
+  }
+
+  eliminar(id:number){
+        this.personas.splice(id,1)
+  }
+
+
+}
